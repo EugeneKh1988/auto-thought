8b7a4af4354fb89ac2c23290b500e7a6bc9bea2d
Evgeniy <khiznyak.1988@gmail.com>
2026-03-22 15:06:50 +0200

    installed lib

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,15 +13,15 @@ fn greet() -> String {
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    let migrations = load_migrations();
-    tauri::Builder::default()
-        .plugin(
-            tauri_plugin_sql::Builder::default()
-                .add_migrations("sqlite:database.db", migrations)
-                .build(),
-        )
-        .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![drizzle_proxy::run_sql, greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+  let migrations = load_migrations();
+  tauri::Builder::default()
+    .plugin(
+      tauri_plugin_sql::Builder::default()
+        .add_migrations("sqlite:database.db", migrations)
+        .build(),
+    )
+    .plugin(tauri_plugin_opener::init())
+    .invoke_handler(tauri::generate_handler![drizzle_proxy::run_sql, greet])
+    .run(tauri::generate_context!())
+    .expect("error while running tauri application");
 }
